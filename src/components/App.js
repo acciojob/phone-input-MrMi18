d62c9a8287c1e55f9c74ff3c3882cdf2d999314e
MrMi18 <is8388508@gmail.com>
2025-07-18 20:09:38 +0530

    Answer Submitted

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [number , setNumber] = useState("")
+const onChangeHandler = (e) =>{
+  let ch = e.target.value;
+  if(isNaN(ch) || number.length>13) return ;
+ 
+    if(number.length===3){
+      setNumber("("+number+")"+ch);
+    }else  if(number.length===6){
+      setNumber(number+"-"+ch);
+    }else {
+      setNumber(number+ch);
+    }
+  
+}
   return (
     <div>
-        
+      <form> 
+        <input type="text" placeholder="(555)555-5555)" value = {number} onChange={(e) => onChangeHandler(e)}/>
+        <button {...number.length!==13?"disabled":"" } onClick={() => setNumber("")}>Submit</button>
+      </form>
     </div>
   )
 }
